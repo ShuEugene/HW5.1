13f0768d8c6165cf38b129e011195c1c586413e6
ShuEugene <shueugene@hotmail.com>
2022-09-05 18:11:20 +0300

    HW5.1.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 //  Домашнее залание к Уроку 5 «Циклы» (Части 1-й).
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+//  ДЗ 5.1.1
+        {
+            for (byte i = 1; i <= 10; i ++) System.out.printf("%d ", i);
+            System.out.println();
+        }
 
     }
 }
