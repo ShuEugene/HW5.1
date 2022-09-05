b54f94c39db6d48244e47b7976708a18e8bff2c2
ShuEugene <shueugene@hotmail.com>
2022-09-05 18:30:46 +0300

    HW5.1.4 complete.

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,11 @@ public class Main {
             for (byte i = 2; i <= 17; i ++) if ((i % 2) == 0) System.out.printf("%d ", i);
             System.out.println();
         }
+
+//  ДЗ 5.1.4
+        {
+            for (byte i = 10; i >= -10; i --) System.out.printf("%d ", i);
+            System.out.println();
+        }
     }
 }
