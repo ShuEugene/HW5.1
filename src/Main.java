3227a618ea3103d1e44736f6d5a3a5b0c8800fa2
ShuEugene <shueugene@hotmail.com>
2022-09-06 09:49:19 +0300

    HW5.1.5 complete.

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,36 @@ public class Main {
         System.out.println();
 //  ДЗ 5.1.1
         {
-            for (byte i = 1; i <= 10; i ++) System.out.printf("%d ", i);
+            for (byte i = 1; i <= 10; i++) System.out.printf("%d ", i);
             System.out.println();
         }
 
 //  ДЗ 5.1.2
         {
-            for (byte i = 10; i >= 1; i --) System.out.printf("%d ", i);
+            for (byte i = 10; i >= 1; i--) System.out.printf("%d ", i);
             System.out.println();
         }
 
 //  ДЗ 5.1.3
         {
-            for (byte i = 2; i <= 17; i ++) if ((i % 2) == 0) System.out.printf("%d ", i);
+            for (byte i = 2; i <= 17; i++) if ((i % 2) == 0) System.out.printf("%d ", i);
             System.out.println();
         }
 
 //  ДЗ 5.1.4
         {
-            for (byte i = 10; i >= -10; i --) System.out.printf("%d ", i);
+            for (byte i = 10; i >= -10; i--) System.out.printf("%d ", i);
             System.out.println();
+        }
+
+//  ДЗ 5.1.5
+        {
+            int startYear = 1904, endYear = 2096, i;
+            System.out.printf("Високосные годы: ");
+            for (i = startYear; i < ((((endYear / 4) - (endYear % 4)) * 4) - 4); i++)
+                if (((i % 4) == 0 && (i % 100) != 0) || (i % 400) == 0)
+                    System.out.printf("%d, ", i);
+            System.out.println((i + 4) + ".");
         }
     }
 }
