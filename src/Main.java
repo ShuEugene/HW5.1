0b4fa59e608e7fef994ed3365e0a16848036b576
ShuEugene <shueugene@hotmail.com>
2022-09-06 10:02:00 +0300

    HW5.1.7 complete.

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,25 @@ public class Main {
 //  ДЗ 5.1.1
         {
             for (byte i = 1; i <= 10; i++) System.out.printf("%d ", i);
-            System.out.println();
+            System.out.println("\n");
         }
 
 //  ДЗ 5.1.2
         {
             for (byte i = 10; i >= 1; i--) System.out.printf("%d ", i);
-            System.out.println();
+            System.out.println("\n");
         }
 
 //  ДЗ 5.1.3
         {
             for (byte i = 2; i <= 17; i++) if ((i % 2) == 0) System.out.printf("%d ", i);
-            System.out.println();
+            System.out.println("\n");
         }
 
 //  ДЗ 5.1.4
         {
             for (byte i = 10; i >= -10; i--) System.out.printf("%d ", i);
-            System.out.println();
+            System.out.println("\n");
         }
 
 //  ДЗ 5.1.5
@@ -33,12 +33,19 @@ public class Main {
             for (i = startYear; i < ((((endYear / 4) - (endYear % 4)) * 4) - 4); i++)
                 if (((i % 4) == 0 && (i % 100) != 0) || (i % 400) == 0)
                     System.out.printf("%d, ", i);
-            System.out.println((i + 4) + ".");
+            System.out.println((i + 4) + ".\n");
         }
 
 //  ДЗ 5.1.6
         {
             for (byte i = 7; i <=98; i += 7) System.out.printf("%d ", i);
+            System.out.println("\n");
+        }
+
+//  ДЗ 5.1.7
+        {
+            for (int i = 1; i <= 512; i *= 2) System.out.printf("%d ", i);
+            System.out.println("\n");
         }
 
         System.out.println();
